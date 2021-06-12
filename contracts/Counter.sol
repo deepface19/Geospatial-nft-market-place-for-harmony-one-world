3e43d26c0618369461eb600eca604c111808a7c7
deepface19 <59292798+deepface19@users.noreply.github.com>
2021-06-12 10:20:33 +0700

    Create Counter.sol

---
+++ contracts/Counter.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.22;
+
+contract Counter {
+    uint256 private count = 0;
+    uint256 moneyStored = 0;
+
+    function incrementCounter() public {
+        count += 1;
+    }
+    function decrementCounter() public {
+        count -= 1;
+    }
+
+    function addMoney() payable public {
+        moneyStored += msg.value;
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
+    function getMoneyStored() public view returns (uint256){
+        return moneyStored;
+    }
+}
