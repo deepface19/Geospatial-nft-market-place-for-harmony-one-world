bfb3333ce07714e8c9919cd10a0e4047f38cd8f4
deepface19 <59292798+deepface19@users.noreply.github.com>
2021-07-25 15:06:35 +0700

    Add files via upload

---
+++ Public/web/config.js
@@ -0,0 +1,30 @@
+// Config
+
+const config = {
+	CHAINID      : 1,
+	CHAINTYPE    : 'hmy',
+	NETWORK      : 'mainnet',
+	NETURL       : 'https://api.harmony.one/',
+	GATEWAY      : 'https://ipfs.io/ipfs/',
+	EXPLORER     : 'https://explorer.harmony.one/',
+	GASPRICE     : 3000000000,
+	GASLIMIT     :   10000000,
+	BuyerFees    : 2,  // 2%
+	SellerFees   : 2,  // 2%
+	AuthorFees   : 10, // 10 ONE
+	Market       : '0x1d89bc60cd482ddfae8208e6a14d6c185c2095a1',
+	Auctions     : '0x762f89c3847658e8ff2c55281aa64fccad87b7cb',
+	Operator     : '0x9a577a8f4f11a0e6abe59cb42133e13424bb90a1',
+	TransferProxy: '0xeda939305ffead73a1aa285a545b56a719b94990',
+	Collections  : {
+		image : '0x1548c6227cbd78e51eb0a679c1f329b9a5a99beb',
+		model : '0x4F1e81F6736b04a75E6A552A30Aa32787566C84d',
+		audio : '0x9f407c15f859277e6654b775cb410bd4aad7fe1f',
+		music : '0xbde650853b535d738ce67f1bdeb335e38834a9e9',
+		video : '0x474d8fd12780fbe2b7b7bd74eb326bb75ded91d8',
+		movie : '0x5d1d9a2cd71d0d03bfc7ba234a4006be38b64362',
+		book  : '0x51f6290510be3c802471e27f0843a3a54a8226df',
+		domain: '0x09f2e816d9a084e1e049713a9ce479e0b1278009',
+		text  : '0x47e50e7056994e8115820d101ffe5ce69cfa77b0'
+	}
+}
--- Public/web/main.js
+++ Public/web/main.js
@@ -1,6 +1,111 @@
+// DAVINCI
+
+const FlexSlider = {
+	num_items: document.querySelectorAll(".slide").length,
+	current: 1,
+
+	init: function() {
+		// set CSS order of each item initially
+		document.querySelectorAll(".slide").forEach(function(element, index) {
+			element.style.order = index+1;
+		});
+
+		this.addEvents();
+	},
+
+	addEvents: function() {
+		var that = this;
+
+		// click on move item button
+		//document.querySelector("#move-button").addEventListener('click', () => {
+		//	this.goNext();
+		//});
+
+		// after each item slides in, slider container fires transitionend event
+		document.querySelector("#slider-box").addEventListener('transitionend', () => {
+			this.changeOrder();
+		});
+	},
+
+	changeOrder: function() {
+		// change current position
+		if(this.current == this.num_items)
+			this.current = 1;
+		else 
+			this.current++;
+
+		let order = 1;
+
+		// change order from current position till last
+		for(let i=this.current; i<=this.num_items; i++) {
+			document.querySelector(".slide[data-position='" + i + "']").style.order = order;
+			order++;
+		}
+
+		// change order from first position till current
+		for(let i=1; i<this.current; i++) {
+			document.querySelector(".slide[data-position='" + i + "']").style.order = order;
+			order++;
+		}
+
+		// translate back to 0 from -100%
+		// we don't need transitionend to fire for this translation, so remove transition CSS
+		document.querySelector("#slider-box").classList.remove('slider-transition');
+		document.querySelector("#slider-box").style.transform = 'translateX(0)';
+	},
+
+	goNext: function() {
+		// translate from 0 to -100% 
+		// we need transitionend to fire for this translation, so add transition CSS
+		document.querySelector("#slider-box").classList.add('slider-transition');
+		document.querySelector("#slider-box").style.transform = 'translateX(-100%)';
+		setTimeout(FlexSlider.goNext, 6000);
+	}
+};
+
+FlexSlider.init();
+setTimeout(FlexSlider.goNext, 8000);
+//setTimeout(FlexSlider.goNext, 2000);
+//setTimeout(FlexSlider.goNext, 4000);
+
+
+async function enableMetamask() {
+	// mainnet = 0x63564c40
+	// testnet = 0x6357d2e0 
+	let shardNum  = 0;
+	let chainId   = config.CHAINID==1?1666600000:1666700000;
+	let chainName = config.CHAINID==1?'Harmony Mainnet':'Harmony Testnet';
+	let chainURL  = config.NETURL;
+	let chainExp  = config.EXPLORER;
+
+	try {
+		let web3 = new Web3(window.ethereum);
+		let list = await web3.eth.getAccounts();
+		if(!list || list.length<1){ alert('Wallet not connected'); return; }
+		let address = list[0];
+	    let params  = [
+		    {
+		        chainId: '0x' + Number(chainId + shardNum).toString(16),
+		        chainName: chainName,
+		        nativeCurrency: { name: 'ONE', symbol: 'ONE', decimals: 18 },
+		        rpcUrls: [chainURL],
+		        blockExplorerUrls: [chainExp],
+		    },
+		    address,
+		];
+	    let result = await window.ethereum.request({ method: 'wallet_addEthereumChain', params: params });
+	    console.log('Metamask result', result);
+	} catch(ex) {
+	    console.log('Metamask error', ex);
+	    alert('Error enabling Harmony Network on Metamask: '+ex.message);
+	}
+}
+
 async function main() {
-	startgeospatialnftmarketplace();
+	startDavinci();
 	//extra
 }
 
 window.onload = main
+
+// end
